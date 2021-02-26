c9c3a406bb1858c700de63f5afbf15ecee06ab07
jiaxing98 <60844861+jiaxing98@users.noreply.github.com>
2021-02-26 23:23:19 +0800

    fix: added directedURL

--- ServerSideAssignment/src/utilities/ValidateManageLogic.java
+++ ServerSideAssignment/src/utilities/ValidateManageLogic.java
@@ -17,10 +17,11 @@ public class ValidateManageLogic {
 
 	// this method is used to notify a user that a record has been updated and to
 	// redirect to another page
-	public static void navigateJS(PrintWriter out) {	//subject to change
+	public static void navigateJS(PrintWriter out, String directedURL) {	//subject to change
 		out.println("<SCRIPT type=\"text/javascript\">");
 		out.println("alert(\"Record has been updated and url will be redirected\")");
 		out.println("window.location.assign(\"PaginationServlet?currentPage=1&recordsPerPage=70&keyword=\")");
+		out.println("window.location.assign(\"" + directedURL + "?currentPage=1&recordsPerPage=70&keyword=\")");
 		out.println("</SCRIPT>");
 	}
 
